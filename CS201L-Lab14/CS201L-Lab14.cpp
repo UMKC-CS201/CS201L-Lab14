3d8c3c1c240c42eae88a67c79e0f20eb5534273a
Joan Gladbach <jjgr29@umsystem.edu>
2024-11-12 09:43:13 -0600

    Add project files.

---
+++ CS201L-Lab14/CS201L-Lab14.cpp
@@ -0,0 +1,11 @@
+// CS201L-Lab14.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Using Maps to See Patterns\n";
+}
+ 
